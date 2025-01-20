56fc323c963619dca500cf1c377fd2d958069b4f
Adam <adam.p.kulczycki@gmail.com>
2025-01-20 23:48:06 +0100

    Fix MX_ADC1_Init to include 4 ADC_Channels & remove unnecessary inits

--- Core/Inc/temperature_control.h
+++ Core/Inc/temperature_control.h
@@ -14,12 +14,12 @@
 
 #define THERMISTOR_GPIO_PORT      GPIOC
 #define THERMISTOR_GPIO_PIN       GPIO_PIN_2
-#define THERMISTOR_ADC_CHANNEL    ADC_CHANNEL_10
+#define THERMISTOR_ADC_CHANNEL    ADC_CHANNEL_12
 
 // Thermistor parameters
 #define THERMISTOR_R25             (10000.0f)
 #define THERMISTOR_B_COEFFICIENT   (3950.0f)
-#define THERMISTOR_REF_TEMPERATURE (298.15f)
+#define THERMISTOR_REF_TEMPERATURE (298.15f) //TODO
 
 // Target & Tolerance
 #define HEAT_EXPECTED_TEMP         (25.0f)
@@ -32,53 +32,15 @@ extern ADC_HandleTypeDef hadc1;
 
 void Heater_On(void)
 {
+	printf("switching heater on\n");
     HAL_GPIO_WritePin(HEATER_GPIO_PORT, HEATER_GPIO_PIN, GPIO_PIN_SET);
 }
 
 void Heater_Off(void)
 {
+	printf("switching heater off\n");
     HAL_GPIO_WritePin(HEATER_GPIO_PORT, HEATER_GPIO_PIN, GPIO_PIN_RESET);
 }
-
-
-/* Configure the ADC channel for the thermistor (PC2 => ADC_CHANNEL_10) */
-void Thermistor_ADC_Init(void)
-{
-    ADC_ChannelConfTypeDef sConfig = {0};
-    sConfig.Channel = THERMISTOR_ADC_CHANNEL;
-    sConfig.Rank = 1;      // Single conversion
-    sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
-    sConfig.Offset = 0;
-
-    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-    {
-        // You might want to handle errors differently
-        Error_Handler();
-    }
-}
-
-
-void Heater_Init(void)
-{
-    // ----------------------------
-    // 1) Configure PC12 for heater control
-    // ----------------------------
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = HEATER_GPIO_PIN;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(HEATER_GPIO_PORT, &GPIO_InitStruct);
-
-    // Start with heater off
-    Heater_Off();
-
-    // ----------------------------
-    // 2) Configure ADC channel 10 (PC2) once
-    // ----------------------------
-    Thermistor_ADC_Init();
-}
-
 
 
 /* Single conversion read of the thermistor channel, returning temp in Celsius */
@@ -109,6 +71,8 @@ float Read_Temperature_Celsius(void)
     float invT = (term / THERMISTOR_B_COEFFICIENT) + (1.0f / THERMISTOR_REF_TEMPERATURE);
     float tempKelvin = 1.0f / invT;
     float tempCelsius = tempKelvin - 273.15f;
+
+    printf("temp: %f\n", tempCelsius);
 
     return tempCelsius;
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -488,51 +488,64 @@ void SystemClock_Config(void)
   * @param None
   * @retval None
   */
-static void MX_ADC1_Init(void)
+void MX_ADC1_Init(void)
 {
-
-  /* USER CODE BEGIN ADC1_Init 0 */
-
-  /* USER CODE END ADC1_Init 0 */
-
   ADC_ChannelConfTypeDef sConfig = {0};
 
-  /* USER CODE BEGIN ADC1_Init 1 */
-
-  /* USER CODE END ADC1_Init 1 */
-
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-  */
+  //--- ADC Config ---
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ScanConvMode = ENABLE;              // enable scanning
   hadc1.Init.ContinuousConvMode = DISABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.NbrOfConversion = 4;                // total 4 channels
   hadc1.Init.DMAContinuousRequests = DISABLE;
   hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();
   }
 
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
+  //--- Channel 10 ---
   sConfig.Channel = ADC_CHANNEL_10;
   sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN ADC1_Init 2 */
 
-  /* USER CODE END ADC1_Init 2 */
+  //--- Channel 11 ---
+  sConfig.Channel = ADC_CHANNEL_11;
+  sConfig.Rank = 2;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
+  //--- Channel 12 ---
+  sConfig.Channel = ADC_CHANNEL_12;
+  sConfig.Rank = 3;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  //--- Channel 13 ---
+  sConfig.Channel = ADC_CHANNEL_13;
+  sConfig.Rank = 4;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
